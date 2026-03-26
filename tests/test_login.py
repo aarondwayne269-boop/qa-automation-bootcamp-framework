bb9328c5f2bbdc6176da74c63e91a9cb704303ae
aarondwayne269-boop <aarondwayne269@gmail.com>
2026-03-26 03:02:55 -0500

    Update test_login.py

--- tests/test_login.py
+++ tests/test_login.py
@@ -1,2 +1,9 @@
-def test_homepage_title():
-    assert "Example" in "Example Domain"
+from selenium import webdriver
+
+def test_open_page():
+    driver = webdriver.Chrome()
+    driver.get("https://example.com")
+
+    assert "Example" in driver.title
+
+    driver.quit()
